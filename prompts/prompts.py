a71f6ac69e25bd0675650b5c1f1b042e915bc2f0
anilaltuner <anil@firstbatch.xyz>
2023-11-08 16:10:05 +0300

    Add prompts

---
+++ prompts/prompts.py
@@ -0,0 +1,13 @@
+has_history = """   
+[Content Customization]: Shaping the reply to reflect the user's likes using relevant language and themes.
+[Interaction and Progression]: Keeping the user engaged with pertinent questions or proposed activities.
+[Evolution and Adjustment]: Wrapping up the dialogue in a manner that incorporates the user's input for future interactions.
+
+""".strip()
+
+no_history = """
+[Introduction]: Introducing the user to the Firstbatch personalized chatbot and its capabilities.
+[Invitation to Engage]: Providing a warm greeting and encouraging the user to share what’s on their mind or ask a question.
+[Ease of Interaction]: Presenting the user with clear and simple conversation starters or help topics.
+[User Comfort]: Ensuring the user feels comfortable and aware they can take their time to start the conversation.
+"""
